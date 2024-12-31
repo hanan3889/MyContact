1042abd6b28480f449b6fe3c4e550862e05405c0
hanan3889 <rebaia_hanan@yahoo.fr>
2024-12-31 12:52:17 +0100

    add Ville and Service to grid

--- MyContact/Models/Salaries.cs
+++ MyContact/Models/Salaries.cs
@@ -9,12 +9,21 @@ namespace MyContact.Models
     public class Salaries
     {
         public int Id { get; set; }
-        public string Nom { get; set; }
-        public string Prenom { get; set; }
-        public string TelephoneFixe { get; set; }
-        public string TelephonePortable { get; set; }
-        public string Email { get; set; }
+        public required string Nom { get; set; }
+        public string? Prenom { get; set; }
+        public string? TelephoneFixe { get; set; }
+        public string? TelephonePortable { get; set; }
+        public string? Email { get; set; }
+
+        // Foreign Key
         public int ServiceId { get; set; }
+        public Services? Service { get; set; }
+
         public int SiteId { get; set; }
+        public Sites? Site { get; set; }
+
+        public string? ServiceNom => Service?.Nom;
+        public string? SiteVille => Site?.Ville;
     }
+
 }
