2d02c4c5643777d72da908a8993469e13a980f89
hanan3889 <rebaia_hanan@yahoo.fr>
2025-01-24 19:25:18 +0100

    add sitesservice file

---
+++ MyContact/Services/SitesService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using MyContact.Models;
+
+namespace MyContact.Services
+{
+    public class SitesService
+        //Permet d appeler l'API
+    {
+        private readonly HttpClient _httpClient;
+
+        public SitesService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<Salaries>> GetSalariesByCityAsync(string ville)
+        {
+            var response = await _httpClient.GetAsync($"https://localhost:5001/api/Sites/get/name/{ville}");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var salaries = JsonSerializer.Deserialize<List<Salaries>>(content);
+
+            return salaries;
+        }
+    }
+}
