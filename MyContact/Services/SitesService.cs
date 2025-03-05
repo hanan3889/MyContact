11d05b7c898750976b4d99485cabd08e7d4c27e0
hanan3889 <rebaia_hanan@yahoo.fr>
2025-03-05 17:03:45 +0100

    modify name of column

--- MyContact/Services/SitesService.cs
+++ MyContact/Services/SitesService.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
+
 using MyContact.Models;
 
 namespace MyContact.Services
