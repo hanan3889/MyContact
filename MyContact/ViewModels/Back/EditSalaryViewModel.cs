b975f75d946f88ebdd88e6ad12dafbd2bbfcc19c
hanan3889 <rebaia_hanan@yahoo.fr>
2025-03-12 12:36:49 +0100

    add site and service

--- MyContact/ViewModels/Back/EditSalaryViewModel.cs
+++ MyContact/ViewModels/Back/EditSalaryViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using MyContact.Commands;
@@ -12,6 +13,31 @@ namespace MyContact.ViewModels.Back
         private readonly SalariesService _salariesService;
         private Salaries _salary;
         private Salaries _editedSalary;
+
+        public ObservableCollection<ServicesModel> Services { get; set; } = new();
+        public ObservableCollection<Sites> Sites { get; set; } = new();
+
+        private ServicesModel? _selectedService;
+        public ServicesModel? SelectedService
+        {
+            get => _selectedService;
+            set
+            {
+                _selectedService = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Sites? _selectedSite;
+        public Sites? SelectedSite
+        {
+            get => _selectedSite;
+            set
+            {
+                _selectedSite = value;
+                OnPropertyChanged();
+            }
+        }
 
         public Salaries Salary
         {
@@ -51,11 +77,49 @@ namespace MyContact.ViewModels.Back
                 Prenom = salary.Prenom,
                 TelephoneFixe = salary.TelephoneFixe,
                 TelephonePortable = salary.TelephonePortable,
-                Email = salary.Email
+                Email = salary.Email,
+                ServiceId = salary.ServiceId,
+                SiteId = salary.SiteId
             };
 
             SaveCommand = new RelayCommand(async (param) => await Save(), (param) => CanSave());
             CancelCommand = new RelayCommand((param) => Cancel());
+
+            _ = LoadData();
+        }
+
+        private async Task LoadData()
+        {
+            try
+            {
+                var services = await _salariesService.GetServicesAsync();
+                var sites = await _salariesService.GetSitesAsync();
+
+                if (services.Count == 0 || sites.Count == 0)
+                {
+                    // Gérer l'erreur si nécessaire
+                    return;
+                }
+
+                Services.Clear();
+                foreach (var service in services)
+                {
+                    Services.Add(service);
+                }
+
+                Sites.Clear();
+                foreach (var siteItem in sites)
+                {
+                    Sites.Add(siteItem);
+                }
+
+                SelectedService = Services.FirstOrDefault(s => s.Id == EditedSalary.ServiceId);
+                SelectedSite = Sites.FirstOrDefault(s => s.Id == EditedSalary.SiteId);
+            }
+            catch (Exception ex)
+            {
+                // Gérer l'erreur si nécessaire
+            }
         }
 
         private async Task Save()
@@ -66,6 +130,8 @@ namespace MyContact.ViewModels.Back
             Salary.TelephoneFixe = EditedSalary.TelephoneFixe;
             Salary.TelephonePortable = EditedSalary.TelephonePortable;
             Salary.Email = EditedSalary.Email;
+            Salary.ServiceId = SelectedService?.Id ?? 0;
+            Salary.SiteId = SelectedSite?.Id ?? 0;
 
             bool success = await _salariesService.UpdateSalaryAsync(Salary);
             if (success)
